1f9ac294e0351ad26741fb274180f23cc995dd8b
KenTra222 <Kentraviousc@gmail.com>
2023-05-23 05:17:59 +0000

    added models

--- bb_landingpage/src/App.jsx
+++ bb_landingpage/src/App.jsx
@@ -9,17 +9,21 @@ function App() {
 
   return (
     <div className='min-w-full h-screen flex items-stretch'>
-    <section className='bg-amber-50 w-1/2 h-full'>
-      <div>
+    <section className='bg-amber-500 w-1/2 h-full flex flex-col items-center justify-evenly'>
+      
+   
 
-        <h1 className=''> hello you guys!!</h1>
-      </div>
+        <h1 className='text-5xl'> Gigibite</h1>
+        <div className='bg-green-900 w-5/6 h-32 flex flex-col items-center justify-evenly'>
+          <p className='text-white'>20% off on all orders  over $200 and more </p>
+          <button className='bg-black text-white w-28 h-8 rounded'>Shop now</button>
+        </div>
+       
 
     </section>
-    <section className='bg-slate-950 w-1/2 h-full'>
-
+    <section className=' w-1/2 h-full'>
+    <p>Buy, Sell, or Trade your the latest tech!</p>
       <Canvas> 
-        
         <Experience/>
       </Canvas>
     </section>
--- bb_landingpage/src/Components/Experience.jsx
+++ bb_landingpage/src/Components/Experience.jsx
@@ -1,18 +1,36 @@
-import React from 'react'
-import { useFBX, Center } from '@react-three/drei'
+import React, {useEffect, useRef, useState} from 'react'
+import { useFBX, Center,  } from '@react-three/drei'
+import { useFrame } from '@react-three/fiber';
+import { motion } from "framer-motion-3d"
 
 
 const Experience = () => {
 
-   const burger = useFBX('/models/burger.fbx')
-   console.log(burger);
+    const burgerRef = useRef()
+
+
+    const headset = useFBX('/models/headset/headset.fbx')
+    const phone = useFBX('/models/phone/Smartphone.fbx')
+    
+ 
+
+   useFrame((state)=>{
+        const time = state.clock.getElapsedTime()
+        const y = Math.sin(time)
+   })
    
   return <>
+  
     <pointLight position={[0, 4, 0]}/>
     <ambientLight/>
     <Center>
+     
 
-        <primitive object={burger} scale={0.0075}/>
+         
+      
+        <primitive object={phone} scale={0.25}>
+            <meshBasicMaterial color={'hotpink'}/>
+        </primitive>
     </Center>
     </>
 }
